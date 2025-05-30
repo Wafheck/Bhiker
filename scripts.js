d5639ba82548ac46df1345fdc8096a68aa98691c
Wafheck <173989399+Wafheck@users.noreply.github.com>
2025-05-30 07:09:09 +0000

    Initial commit, html skeleton

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const browseBtn = document.querySelector(".browse-button");
+
+browseBtn.addEventListener("mouseover", () => {
+  browseBtn.textContent = "BROWSE🔍";
+});
+
+browseBtn.addEventListener("mouseout", () => {
+  browseBtn.textContent = "BROWSE🔍";
+});
