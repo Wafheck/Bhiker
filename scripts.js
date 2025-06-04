9ed282e508a18e972b536ef4cddaa3cc534cadfb
Mohamed Wafiq <Wafheck@users.noreply.github.com>
2025-06-04 13:07:49 +0530

    Fixed merge between main and master

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const browseBtn = document.querySelector(".browse-button");
-
-browseBtn.addEventListener("mouseover", () => {
-  browseBtn.textContent = "BROWSE🔍";
-});
-
-browseBtn.addEventListener("mouseout", () => {
-  browseBtn.textContent = "BROWSE🔍";
-});
