7d2f29c6883221b25019503a78e63951e2c7cc1f
Mohamed Wafiq <Wafheck@users.noreply.github.com>
2025-08-04 17:12:58 +0530

    Fixed Render Setup

--- bhiker-backend/index.js
+++ bhiker-backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001' , 'https://www.bhiker.me'],
     credentials: true
 }));
 
